8d40b9429cedcf370b205461765d1dfc98ee0f29
Platypus84 <mailtojulian@gmx.de>
2025-09-10 22:15:46 +0200

    update_03

--- lib/common/theme/theme.dart
+++ lib/common/theme/theme.dart
@@ -2,15 +2,15 @@ import 'package:flutter/material.dart';
 
 final ThemeData appTheme = ThemeData(
   useMaterial3: true,
-  scaffoldBackgroundColor: Color.fromARGB(255, 43, 44, 47),
+  scaffoldBackgroundColor: Color.fromARGB(255, 30, 31, 36),
   appBarTheme: AppBarTheme(
     titleTextStyle: TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
     foregroundColor: Color.fromARGB(255, 255, 255, 255),
-    backgroundColor: Color.fromARGB(255, 43, 44, 47),
+    backgroundColor: Color.fromARGB(255, 57, 60, 70),
   ),
   textTheme: TextTheme(
     headlineMedium: TextStyle(
-      color: Color.fromARGB(255, 78, 202, 255),
+      color: Color.fromARGB(255, 78, 161, 255),
       fontSize: 18,
       fontWeight: FontWeight.bold,
     ),
